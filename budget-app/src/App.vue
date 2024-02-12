9bffcca0358262d3dd3c0ebea7423e96244d02ce
Pavel-VV <vahonin7@mail.ru>
2024-02-12 22:13:53 +0300

    finish project, start home work

---
+++ budget-app/src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <FormBudget @submitForm="onFormSubmit"/>
+    <TotalBalance :total="totalBalanceList"/>
+    <BudgetList :list="list" @deleteItemList="onDeleteItem"/>
+  </div>
+</template>
+
+<script>
+import BudgetList from '@/components/BudgetList';
+import TotalBalance from '@/components/TotalBalance';
+import FormBudget from '@/components/FormBudget'
+
+export default {
+  name: 'App',
+  components: {
+    BudgetList,
+    TotalBalance,
+    FormBudget,
+  },
+  data: () => ({
+    list: {
+      1: {
+        type: 'INCOME',
+        value: 100,
+        comment: 'Some comments',
+        id: 1,
+      },
+      2: {
+        type: 'OUTCOME',
+        value: -50,
+        comment: 'Some outcome comments',
+        id: 2,
+      },
+    },
+  }),
+  computed: {
+    totalBalanceList() {
+      return Object.values(this.list).reduce((acc, item) => {return acc + item.value}, 0)
+    },
+  },
+  methods: {
+    onDeleteItem(id) {
+      this.$delete(this.list, id)
+    },
+    onFormSubmit(data) {
+      console.log(data);
+      const newObj = {
+        ...data,
+        id: String(Math.random()),
+      };
+      console.log(newObj)
+      this.$set(this.list, newObj.id, newObj)
+    },
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ budget-app/src/components/TotalBalance.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="total-value">Total Balance: {{total}} </div>
+</template>
+
+<script>
+export default {
+  name: "TotalBalance",
+  props: {
+    total: {
+      type: Number,
+      default: 0,
+    },
+  },
+}
+
+</script>
+
+<style scoped>
+.total-value {
+  padding: 20px;
+  font-size: 26px;
+  text-transform: uppercase;
+  text-align: center;
+}
+
+</style>
