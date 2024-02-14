640115fa9f8def43ab6d4559a44093d1c5a52106
Pavel-VV <vahonin7@mail.ru>
2024-02-14 23:30:33 +0300

    finish homework, version element-ui Diaolog

--- budget-app/src/components/BudgetListItem.vue
+++ budget-app/src/components/BudgetListItem.vue
@@ -2,13 +2,26 @@
   <div class="list-item">
     <span class="budget-comment"> {{ listItem.comment }} </span>
     <span class="budget-value"> {{ listItem.value }} </span>
-    <ElButton type="danger" size="mini" @click="deleteItem(listItem.id)">Delete</ElButton>
+    <ElButton type="danger" size="mini" @click="dialogVisible = true">Delete</ElButton>
+    <ElDialog
+      :visible.sync="dialogVisible"
+      width="30%"
+      :show-close = false>
+      <span>Вы точно хотите удалить запись?</span>
+      <span slot="footer" class="dialog-footer">
+        <ElButton @click="dialogVisible = false">Отмена</ElButton>
+        <ElButton type="primary" @click="deleteItem(listItem.id)">Подтвердить</ElButton>
+      </span>
+    </ElDialog>
   </div>
 </template>
 
 <script>
 export default {
   name: 'BudgetListItem',
+  data: () => ({
+    dialogVisible: false,
+  }),
   props: {
     listItem: {
       type: Object,
@@ -17,6 +30,8 @@ export default {
   },
   methods: {
     deleteItem(id) {
+      // if(!confirm('Вы уверены?')) return;
+      this.dialogVisible = false;
       this.$emit('deleteItemId', id) // прокидываем в компоненту budgetList id кликнутого item
     }
   }
